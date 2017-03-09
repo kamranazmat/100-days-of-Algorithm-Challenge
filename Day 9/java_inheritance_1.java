36989f9411ff74b79e37fdad792d3d1013bb51a1
kamranazmat <azmat.kamran@gmail.com>
2017-03-09 15:23:33 +0000

    inheritance - extends

--- Day 9/java_inheritance_1.java
+++ Day 9/java_inheritance_1.java
@@ -4,9 +4,12 @@
     }
 }
 
-class Bird extends Animal {
-    void fly() {
-        System.out.println("I am flying");
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+    void sing() {
+        System.out.println("I am singing");
     }
 }
 
